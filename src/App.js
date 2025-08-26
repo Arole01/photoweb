f1c372626f4d7d81d904fb2756ba05c2f4a2fd26
aninkud2 <adetadeservices@gmail.com>
2025-08-26 08:59:30 +0100

    i changed some pictures

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Passion from "./Components/Passion";
 import Service from "./Components/Service";
 import appCss from "./Styles/Styles.css"
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
+ 
 function App() {
   return (
     <div className="App">
